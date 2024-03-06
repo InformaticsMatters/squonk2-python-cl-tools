8f275be1b3c371224c6276ef9812baf1cb83a943
Alan Christie <alan.christie@matildapeak.com>
2024-03-06 15:13:25 +0100

    feat: New 'create orgs and units' tool

--- tools/create-organisations-and-units.py
+++ tools/create-organisations-and-units.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 """Creates organisations using a YAML file to define their names and owners.
-The file is simply a list of organisations that have a `name`, and `owner`.
+The file is simply a list of organisations that have a `name`, and `owner`
+with an optional list of `units` with names and billing days (with a default of '3)
+(which are created in the same way).
 """
 import argparse
 from pathlib import Path
@@ -43,47 +45,56 @@ def main(c_args: argparse.Namespace, filename: str) -> None:
     if not org_rv.success:
         console.log(':boom: Failed to get existing organisations')
         sys.exit(1)
-    existing_org_names: List[str] = [org['name'] for org in org_rv.msg['organisations']]
+    existing_org_names: List[str] = []
+    existing_orgs: Dict[str, str] = {}
+    for org in org_rv.msg['organisations']:
+        if org['name'] not in ['Default']:
+            existing_orgs[org['name']] = org['id']
+            existing_org_names.append(org['name'])
 
     # Just read the list from the chosen file
     file_content: str = Path(filename).read_text(encoding='utf8')
     orgs: List[Dict[str, Any]] = yaml.load(file_content, Loader=yaml.FullLoader)
     # Create the organisations one at a time (to handle any errors gracefully)
-    num_orgs: int = 0
-    num_orgs_failed: int = 0
-    num_orgs_skipped: int = 0
     for org in orgs:
-        name: str = org.get('name')
-        if not name:
+        org_name: str = org.get('name')
+        if not org_name:
             console.log(':boom: File has an organisation without a name')
             sys.exit(1)
         owner: str = org.get('owner')
-        if not name:
+        if not owner:
             console.log(':boom: File has an organisation without an owner')
             sys.exit(1)
         # Now try and create the organisation (if it's new)...
-        if name in existing_org_names:
-            console.log(f':white_check_mark: Skipping "{name}" - it already exists')
-            num_orgs_skipped += 1
+        if org_name in existing_org_names:
+            console.log(f':white_check_mark: Skipping organisation "{org_name}" - it already exists')
         else:
-            org_rv: AsApiRv = AsApi.create_organisation(token, org_name=name, org_owner=owner)
+            org_rv: AsApiRv = AsApi.create_organisation(token, org_name=org_name, org_owner=owner)
             if org_rv.success:
-                num_orgs += 1
                 emoji = ':white_check_mark:'
+                existing_orgs[org_name] = org_rv.msg['id']
             else:
-                num_orgs_failed += 1
                 emoji = ':cross_mark:'
             # Log
-            console.log(f'{emoji} {name} ({owner})')
-
-    # Summary
-    if num_orgs:
-        console.log(f'Organisations created {num_orgs}')
-    if num_orgs_skipped:
-        console.log(f'Organisations skipped {num_orgs_skipped}')
-    # Error states
-    if num_orgs_failed:
-        console.log(f'Organisations failed {num_orgs_failed}')
+            console.log(f'{emoji} {org_name} ({owner})')
+        # Units?
+        if 'units' in org:
+            org_rv: AsApiRv = AsApi.get_units(token, org_id=existing_orgs[org_name])
+            existing_unit_names: List[str] = [unit['name'] for unit in org_rv.msg['units']]
+            for unit in org['units']:
+                unit_name: str = unit.get('name')
+                if not unit_name:
+                    console.log(':boom: File has a unit without a name')
+                    sys.exit(1)
+                if unit_name in existing_unit_names:
+                    console.log(f':white_check_mark: Skipping unit "{org_name}/{unit_name}" - it already exists')
+                else:
+                    billing_day: int = unit.get('billing_day', 3)
+                    # Now try and create the unit (if it's new)...
+                    unit_rv: AsApiRv = AsApi.create_unit(token, org_id=existing_orgs[org_name], unit_name=unit_name, billing_day=billing_day)
+                    emoji = ':white_check_mark:' if unit_rv.success else ':cross_mark:'
+                    # Log
+                    console.log(f'  {emoji} {unit_name} (billing day {billing_day})')
 
 
 if __name__ == "__main__":
@@ -91,7 +102,7 @@ if __name__ == "__main__":
     # Parse command line arguments
     parser = argparse.ArgumentParser(
         prog="create-organisations",
-        description="Creates Organisations (from a YAML file). You will need admin privileges to use this tool."
+        description="Creates Organisations and Units (from a YAML file). You will need admin privileges to use this tool."
     )
     parser.add_argument('environment', type=str, help='The environment name')
     parser.add_argument('file', type=str, help='The source file')
